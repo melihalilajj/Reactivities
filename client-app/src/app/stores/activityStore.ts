6a057aa8cea862fb5217deec1f68bd4ced24f52a
melihalilajj <mh47114@ubt-uni.net>
2022-02-21 21:59:21 +0000

    End of Section 11

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import {makeAutoObservable, runInAction} from "mobx";
 import { act } from "react-dom/test-utils";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+import { format } from "date-fns";
 
 
 
@@ -10,7 +11,7 @@ export default class ActivityStore{
   selectedActivity: Activity | undefined = undefined;
   editMode = false;
   loading = false;
-  loadingInitial = true;
+  loadingInitial = false;
 
     constructor(){
         makeAutoObservable(this)
@@ -19,13 +20,13 @@ export default class ActivityStore{
 
     get activitiesByDate(){
          return Array.from(this.activityRegistry.values()).sort((a, b) =>
-                Date.parse(a.date) - Date.parse(b.date));
+                a.date!.getDate() - b.date!.getTime());
     }
 
     get groupedActivities(){
         return Object.entries(
             this.activitiesByDate.reduce((activities, activity) => {
-                const date=activity.date;
+                const date=format (activity.date!, 'dd MMM yyyy');
                 activities[date]=activities[date] ? [...activities[date], activity] : [activity];
                 return activities;
             },{} as {[key: string]: Activity[]} )
@@ -71,7 +72,7 @@ export default class ActivityStore{
 
 
     private setActivity = (activity: Activity) =>{
-        activity.date=activity.date.split('T')[0];
+        activity.date=new Date(activity.date!);
         this.activityRegistry.set(activity.id, activity);
     }
 
